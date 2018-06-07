54d5271bea6eddd355e1dc5ee4b6057ff2b5a595
Ji Barker <Jibarker638@gmail.com>
2018-06-07 23:40:49 +1000

    intital comment

--- javaScript.js
+++ javaScript.js
@@ -1,10 +1,10 @@
 
 function binary_checker() {
     var binary_number = document.getElementById("num1").value;
-    var index = 0;
-    var number_index = 0;
-    var pos;
-    var neg;
+    var index_decimal_point = 0;
+    var counter = 0;
+    var whole_number;
+    var fractional_number;
     var answer;
     var i ;
     //This loop will check whether binary number is vaild
@@ -12,23 +12,23 @@ function binary_checker() {
       //checks whether the element is '1' or '0'
         if (binary_number[i] !== "1" && binary_number[i] !== "0"){
             if (binary_number[i] === "."){
-                index = i;
-                number_index += 1;
+                index_decimal_point = i;
+                counter += 1;
            }else {
              //if not number index will equal 2
-            number_index = 2;
+            counter = 2;
             break}
         }}
 //this is for if the binary number is vaild and has one decimal point
-    if (number_index === 1){
-        pos = binary_number.slice(0,index);
-        neg = binary_number.slice(index+1, binary_number.length);
-        answer = pos_binary_to_decimal(pos) + neg_binary_to_decimal(neg);
+    if (counter === 1){
+        whole_number = binary_number.slice(0,index_decimal_point);
+        fractional_number = binary_number.slice(index_decimal_point+1, binary_number.length);
+        answer = whole_binary_to_decimal(fractional_number) + fractional_binary_to_decimal(whole_number);
         document.getElementById("yeah").innerHTML = answer;
     }
     //this is for if the binary number is a whole number
-    else if (number_index === 0){
-        document.getElementById("yeah").innerHTML = pos_binary_to_decimal(binary_number);
+    else if (counter === 0){
+        document.getElementById("yeah").innerHTML = whole_binary_to_decimal(binary_number);
     }
     else{
     //this is if the binary number has more then one decimal place or contains not a '1' or '0'
@@ -36,32 +36,32 @@ function binary_checker() {
     }
 }
 //this is the function for converting positive binary side to decimal
-function pos_binary_to_decimal(binary_number) {
-    var decimal_number = 0;
+function whole_binary_to_decimal(binary_number) {
+    var whole_decimal = 0;
     var i;
     for (i = 0;i < binary_number.length; i++){
-        decimal_number += (Math.pow(2, i) * (parseInt(binary_number[binary_number.length-1-i])));
+        whole_decimal += (Math.pow(2, i) * (parseInt(binary_number[binary_number.length-1-i])));
     }
-    return decimal_number
+    return whole_decimal
 }
 //this is the function for converting
-function neg_binary_to_decimal(binary_number) {
-    var decimal_number = 0;
+function fractional_binary_to_decimal(binary_number) {
+    var fractional_decimal = 0;
     var i;
     for (i = 0;i < binary_number.length; i++){
-        decimal_number += (Math.pow(2, (-i-1)) * (parseInt(binary_number[i])));
+        fractional_decimal += (Math.pow(2, (-i-1)) * (parseInt(binary_number[i])));
     }
-    return decimal_number
+    return fractional_decimal
 }
 //checks the validation of the decimal number
 function decimal_checker() {
-    var binary_number = document.getElementById("num2").value;
+    var decimal_number = document.getElementById("num2").value;
     var list_numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
     var i;
     var counter = 0;
     for (i = 0; i < binary_number.length; i++) {
-        if (list_numbers.indexOf(binary_number[i]) > -1) {
-            if (binary_number[i] === ".") {
+        if (list_numbers.indexOf(decimal_number[i]) > -1) {
+            if (decimal_number[i] === ".") {
                 counter += 1;
             }
         }
